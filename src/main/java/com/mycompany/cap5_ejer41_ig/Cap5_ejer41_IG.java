355b19b553fb03edc12de0c6568769499c456068
goethe01 <124703630+goethe01@users.noreply.github.com>
2023-04-15 22:31:10 -0500

    Update Cap5_ejer41_IG.java

--- src/main/java/com/mycompany/cap5_ejer41_ig/Cap5_ejer41_IG.java
+++ src/main/java/com/mycompany/cap5_ejer41_ig/Cap5_ejer41_IG.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.cap5_ejer41_ig;
 
-/**
- *
- * @author eduar
- */
+
 public class Cap5_ejer41_IG {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
